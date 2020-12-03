e9ebbab1f5046a96f5bc957fd016ea286c8836e1
Murat Sahin <yazilimogren1111@gmail.com>
2020-12-04 00:15:33 +0100

    user detail added

--- routes/MainRouter.js
+++ routes/MainRouter.js
@@ -2,7 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) =>{
-    res.render('main')
+    res.render('main', {name:req.query.name})
+})
+
+router.post('/', (req, res)=>{
+    res.send('post');
 })
 
 module.exports = router;
--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -2,8 +2,13 @@ const { render } = require('ejs');
 const express = require('express');
 const router = express.Router();
 
+const data =require('../models/data')
+
 router.get('/',(req, res)=>{
-    res.render('user')
+    res.render('user', {users:data.userList, id:''})
 })
 
+router.get('/:id', (req, res)=>{
+    res.render('user', {users:data.userList, id:req.params.id})
+})
 module.exports = router;
